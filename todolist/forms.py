acebd901e8128226ce386ae22402cf546364f211
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-04-03 14:26:31 +0300

    update taskcreate form with tags checklist widget

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from django.forms import DateTimeInput
-from todolist.models import Task
+from todolist.models import Task, Tag
 
 
 class TaskCreateForm(forms.ModelForm):
     deadline = forms.DateTimeField(
         required=False, widget=DateTimeInput(attrs={"type": "datetime-local"})
+    )
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
     )
 
     class Meta:
