37d7246a0a44543b96e09fd822a052eb9f0a6b57
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-04-03 10:42:05 +0300

    update todolist models

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag:
+    name = models.CharField(max_length=63)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True)
+    done_marker = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+
+
+
