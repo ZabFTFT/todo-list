4bd200b05a33f6f538aaefaa62eadd1b8c109d18
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-04-03 11:05:25 +0300

    update views with Task base functionality

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todolist.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+
