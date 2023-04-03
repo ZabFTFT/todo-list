2f891fb079affc3cf229fb6f6bc60e1445c4c603
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-04-03 13:41:41 +0300

    add tag related views and url patterns

--- todolist/urls.py
+++ todolist/urls.py
@@ -6,6 +6,10 @@ from todolist.views import (
     TaskUpdateView,
     TaskDeleteView,
     switch_complete_status,
+    TagListView,
+    TagUpdateView,
+    TagCreateView,
+    TagDeleteView,
 
 )
 
@@ -17,6 +21,12 @@ urlpatterns = [
     path("todolist/<int:pk>/update/", TaskUpdateView.as_view(), name='update-task'),
     path("todolist/<int:pk>/delete/", TaskDeleteView.as_view(), name='delete-task'),
     path("todolist/<int:pk>/switch_status/", switch_complete_status, name="switch-status-task" ),
+
+    path("tagslist/", TagListView.as_view(), name="list-tag"),
+    path("tagslist/create_tag/", TagCreateView.as_view(), name="create-tag"),
+    path("tagslist/<int:pk>/update/", TagUpdateView.as_view(), name="update-tag"),
+    path("tagslist/<int:pk>/delete/", TagDeleteView.as_view(), name="delete-tag"),
+
 
 
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from todolist.models import Task
+from todolist.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
@@ -26,6 +26,23 @@ class TaskDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("todolist:list-task")
 
+class TagListView(generic.ListView):
+    model = Tag
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todolist:list-tag")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todolist:list-tag")
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todolist:list-tag")
 
 def switch_complete_status(request, pk: int):
     task = Task.objects.get(id=pk)
